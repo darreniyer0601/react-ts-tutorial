238d3eb34beec1ba7e3e53b2d2fe46962657dc32
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-05-17 17:41:58 -0400

    added new todo implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 import NewTodo from './components/NewTodo';
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
-      <NewTodo />
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos}/>
     </div>
   );
