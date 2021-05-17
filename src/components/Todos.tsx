a9891aad22d670c9a012eb38dd32e19d9933ca08
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-05-17 17:11:19 -0400

    separated each item into separate component

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 // FC stands for functional component
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
     return (
         <ul>
-            {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+            {props.items.map(item => (
+                <TodoItem key={item.id} text={item.text}/>
+            ))}
         </ul>
     );
 }
