32fccd0b1e69da73dbe12c2d417f9e987807ed88
darreniyer0601 <54214698+darreniyer0601@users.noreply.github.com>
2021-05-17 16:49:00 -0400

    Added todos component with type checking

---
+++ src/components/Todos.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// FC stands for functional component
+const Todos: React.FC<{ items: string[] }> = (props) => {
+    return (
+        <ul>
+            {props.items.map(item => <li key={item}>{item}</li>)}
+        </ul>
+    );
+}
+
+export default Todos;
